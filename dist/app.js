54002e434cbd97e56707c3c46170e2e68029b321
Lê Xuân Danh <79413075+DanhLe1001@users.noreply.github.com>
2022-08-16 23:54:48 +0700

    add3

--- dist/app.js
+++ dist/app.js
@@ -1,1 +1,44 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+let Poke;
+let pokemons = []; // tao ra mang cac phan tu rong
+function template(pokeItem) {
+    return ` <div class="pokemon">
+                <div>ID:${pokeItem.id} </div> 
+                <img src="${pokeItem.image}" alt="${pokeItem.type}">
+            </div> `;
+}
+// ham lay du lieu tu website
+function fetchData(root) {
+    return __awaiter(this, void 0, void 0, function* () {
+        for (let i = 1; i <= 20; i++) {
+            let data = yield fetch(`https://pokeapi.co/api/v2/pokemon/${i}`); // lay thong ten internet let pokemon: any=await
+            let pokemon = yield data.json();
+            console.log(pokemon);
+            let { name: pokename, url } = pokemon.abilities[0].ability;
+            let { front_default: imageUrl } = pokemon.sprites;
+            let { name: type } = pokemon.type[0].type;
+            let poke = {
+                id: i,
+                name: pokename,
+                image: imageUrl,
+                type: type
+            };
+            pokemons.push(poke);
+        }
+        console.log(pokemons);
+    });
+}
+const root = document.getElementById("app");
+console.log(root);
+if (root) {
+    fetchData(root);
+}
--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,54 @@
-class PokemonClass {
-    id:number;
-    name:string;
-    image:string;
-    type:string;
-    constructor(id:number, name:string, image:string, type:string){
-        this.id = id;
-        this.name = name;
-        this.image = image;
-        this.type   = type;
+
+
+let Poke: {
+    id: number;
+    name: string;
+    image: string;
+    type: string;
+};
+let pokemons: {
+    id: number;
+    name: string;
+    image: string;
+    type: string;
+}[] = []; // tao ra mang cac phan tu rong
+
+function template(pokeItem: {
+    id: number;
+    name: string;
+    image: string;
+    type: string;
+}) {
+    return ` <div class="pokemon">
+                <div>ID:${pokeItem.id} </div> 
+                <img src="${pokeItem.image}" alt="${pokeItem.type}">
+            </div> `;
+}
+
+// ham lay du lieu tu website
+async function fetchData(root: HTMLElement) {
+    for (let i = 1; i <= 20; i++) {
+        let data: Response = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}`); // lay thong ten internet let pokemon: any=await
+        let pokemon: any = await data.json();
+        console.log(pokemon);
+
+
+        let { name: pokename, url } = pokemon.abilities[0].ability;
+        let { front_default: imageUrl } = pokemon.sprites;
+        let { name: type } = pokemon.type[0].type;
+        let poke = {
+            id: i,
+            name: pokename,
+            image: imageUrl,
+            type: type
+        }
+        pokemons.push(poke);
     }
+    console.log(pokemons);
+}
+const root = document.getElementById("app");
+console.log(root);
+
+if (root) {
+    fetchData(root);
 }
