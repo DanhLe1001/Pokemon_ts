96f1de8e0a7f041969fab0074f2b81e22f2253b5
Lê Xuân Danh <79413075+DanhLe1001@users.noreply.github.com>
2022-08-16 09:53:25 +0700

    add2

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+class PokemonClass {
+    id:number;
+    name:string;
+    image:string;
+    type:string;
+    constructor(id:number, name:string, image:string, type:string){
+        this.id = id;
+        this.name = name;
+        this.image = image;
+        this.type   = type;
+    }
+}
