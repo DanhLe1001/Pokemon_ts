00f39ec19433ebdb6ee6f26530f2ea1cca02da52
Lê Xuân Danh <79413075+DanhLe1001@users.noreply.github.com>
2022-08-22 14:37:47 +0700

    abccbcb

--- src/test.ts
+++ src/test.ts
@@ -1,18 +1,3 @@
 let pokemon: any = await data.json();
 console.log(pokemon);
 let { name: pokename, url } = pokemon.abilities[0].ability;
-let { front_default: imageUrl } = pokemon.sprites;
-let { name: type } = pokemon.type[0].type;
-
-let poke = new Pokemon(i, pokename, imageUrl, type);
-
-this.pokemons.push(poke);
-}
-console.log(this.pokemons);
-this.shuffle();
-
-this.pokemons.forEach(Element => {
-    root.innerHTML += this.template(Element)
-})
-}
-
