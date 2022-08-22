fac589f1b5756eafe2b0a1d3e59096e2936d7b42
Lê Xuân Danh <79413075+DanhLe1001@users.noreply.github.com>
2022-08-22 13:33:39 +0700

    git commit

--- dist/app.js
+++ dist/app.js
@@ -8,37 +8,54 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-let Poke;
-let pokemons = []; // tao ra mang cac phan tu rong
-function template(pokeItem) {
-    return ` <div class="pokemon">
-                <div>ID:${pokeItem.id} </div> 
-                <img src="${pokeItem.image}" alt="${pokeItem.type}">
-            </div> `;
+//import fetch from 'node-fetch';
+class Pokemon {
+    constructor(id, name, image, type) {
+        this.id = id;
+        this.name = name;
+        this.image = image;
+        this.type = type;
+    }
 }
-// ham lay du lieu tu website
-function fetchData(root) {
-    return __awaiter(this, void 0, void 0, function* () {
-        for (let i = 1; i <= 20; i++) {
-            let data = yield fetch(`https://pokeapi.co/api/v2/pokemon/${i}`); // lay thong ten internet let pokemon: any=await
-            let pokemon = yield data.json();
-            console.log(pokemon);
-            let { name: pokename, url } = pokemon.abilities[0].ability;
-            let { front_default: imageUrl } = pokemon.sprites;
-            let { name: type } = pokemon.type[0].type;
-            let poke = {
-                id: i,
-                name: pokename,
-                image: imageUrl,
-                type: type
-            };
-            pokemons.push(poke);
-        }
-        console.log(pokemons);
-    });
+class PokemonData {
+    constructor() {
+        this.pokemons = [];
+        this.shuffle = () => {
+            for (let i = 0; i < this.pokemons.length; i++) {
+                let j = Math.round(Math.random() * this.pokemons.length);
+                let temp = this.pokemons[i];
+                this.pokemons[i] = this.pokemons[j];
+                this.pokemons[j] = temp;
+            }
+        };
+        this.template = (pokeItem) => {
+            return (` <div class="pokemon" >
+                    <div>ID:${pokeItem.id} </div> 
+                    <img src="${pokeItem.image}" alt="${pokeItem.type}">
+                </div> `);
+        };
+        this.fetchData = (root) => __awaiter(this, void 0, void 0, function* () {
+            for (let i = 1; i <= 20; i++) {
+                let data = yield fetch(`https://pokeapi.co/api/v2/pokemon/${i}`); // lay thong ten internet let pokemon: any=await
+                let pokemon = yield data.json();
+                console.log(pokemon);
+                let { name: pokename, url } = pokemon.abilities[0].ability;
+                let { front_default: imageUrl } = pokemon.sprites;
+                let { name: type } = pokemon.type[0].type;
+                let poke = new Pokemon(i, pokename, imageUrl, type);
+                this.pokemons.push(poke);
+            }
+            console.log(this.pokemons);
+            this.shuffle();
+            this.pokemons.forEach(Element => {
+                root.innerHTML += this.template(Element);
+            });
+        });
+    }
 }
-const root = document.getElementById("app");
-console.log(root);
+//let root = document.getElementById("app");
+const root = document.getElementById('app');
 if (root) {
-    fetchData(root);
+    let header = new PokemonData();
+    header.fetchData(root);
 }
